2aea31dd57e621d52ef57c8d0f1653141a238931
Matias Meno <m@tias.me>
2023-01-10 11:08:19 +0100

    add doc

--- my-app/.storybook/main.js
+++ my-app/.storybook/main.js
@@ -1,45 +1,38 @@
-import { mergeConfig } from 'vite'
-import Icons from 'unplugin-icons/vite'
-import path from 'path'
+import { mergeConfig } from "vite";
+import Icons from "unplugin-icons/vite";
+import path from "path";
 
 /**
  * @type {import('@storybook/sveltekit').StorybookConfig}
  */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-svelte-csf',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
   framework: {
-    name: '@storybook/sveltekit',
+    name: "@storybook/sveltekit",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
   async viteFinal(config) {
     // Merge custom configuration into the default config
     return mergeConfig(config, {
-      // Use the same "resolve" configuration as your app
-      resolve: {
-        alias: {
-          $lib: path.resolve(__dirname, '../src/lib'),
-        },
-      },
       plugins: [
         Icons({
-          compiler: 'svelte',
+          compiler: "svelte",
         }),
       ],
       // Add dependencies to pre-optimization
       optimizeDeps: {
-        include: ['storybook-dark-mode'],
+        include: ["storybook-dark-mode"],
       },
-    })
+    });
   },
-}
+};
 
-export default config
+export default config;
